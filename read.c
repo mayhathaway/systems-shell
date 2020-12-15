26923271a89a7c354772763961a187be11ea0afb
Sasha Wald <sashawald@gmail.com>
2020-12-15 13:59:03 -0500

    added user input for parsing commands

--- read.c
+++ read.c
@@ -18,13 +18,22 @@ char ** parse_args(char *line){
 }
 
 int main(){
-    int f = fork();
-    char line[] = "ls -a -l";
-    char ** args = parse_args(line);
-    if (f){
-        printf("fork\n");
-        execvp(args[0], args);
-        return(0);
+    char line[256];
+    while(1){
+        printf("\nhermit-crab: sasha$ ");
+        if (fgets(line,256,stdin)!=NULL){
+            int len = strlen(line);
+            if (len > 0 && line[len-1] == '\n'){
+                len--;
+                line[len] = '\0';
+            }
+        }
+        //running commands (for now). will need to sep by ; in the future, so making a separate function
+        char ** args = parse_args(line);
+        for(int n=0; args[n] != NULL; n++){
+            execvp(args[0], args);
+        }
+        free(args);
     }
     return(0);
 }
