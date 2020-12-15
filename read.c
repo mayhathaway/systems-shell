b7d6c300d0e5344ea47561b4a7dce64b05cf886d
Sasha Wald <sashawald@gmail.com>
2020-12-15 14:37:52 -0500

    added exit/cd
    no more fork bombing myself

--- read.c
+++ read.c
@@ -10,11 +10,36 @@ char *args[5];
 char ** parse_args(char *line){
     char *copy = line;
     int n = 0;
+    char **args = malloc(sizeof(char *));
     while(copy){
+        args[n]=malloc(sizeof(char));
         args[n]=strsep(&copy, " ");
         n++;
     }
     return(args);
+}
+
+void run_process(char *cmd){
+    char ** args = parse_args(cmd);
+    if (strcmp(args[0],"exit") == 0){
+        exit(0);
+    }
+    else if (strcmp(args[0],"cd") == 0){
+        chdir(args[1]);
+    }
+    else{
+        int f = fork();
+        if (f){
+            int status;
+            wait(&status);
+            if(WEXITSTATUS(status)!=0){
+                printf("%s\n", strerror(WEXITSTATUS(status)));
+            }
+        }
+        else{
+            int n = execvp(args[0],args);
+        }
+    } 
 }
 
 int main(){
@@ -29,11 +54,12 @@ int main(){
             }
         }
         //running commands (for now). will need to sep by ; in the future, so making a separate function
-        char ** args = parse_args(line);
-        for(int n=0; args[n] != NULL; n++){
-            execvp(args[0], args);
+        char **args = parse_args(line);
+        for (int i = 0; args[i]!=NULL; i++){
+            run_process(args[i]);
         }
         free(args);
     }
+    printf("\n");
     return(0);
 }
